0e3c89a30ee8c592a67f96566e836ea7c5f4fd4b
Riley Housden <riley.t.housden@gmail.com>
2024-04-17 16:04:05 -0400

    on_member_remove embed formatting

--- mc_bot/cogs/players.py
+++ mc_bot/cogs/players.py
@@ -52,7 +52,7 @@ class Players(commands.Cog):
     @commands.Cog.listener()
     async def on_member_remove(self, member: discord.Member) -> None:
         logger.info("Member %s left the server, removing player data.", member.name)
-        embed = PlayersEmbed(title="Player Data Removed")
+        embed = PlayersEmbed(title=f"{member.name}'s Player Data Removed")
         try:
             player = self.bot.player_data.get(member.id)
             embed.description = f"Player data for {member.name} has been removed."
